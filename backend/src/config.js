0f78f3847855eeb102f1f31f27836d0489acd20f
Nikhl Kumar <30932185+nikhilkrdwivedi@users.noreply.github.com>
2022-05-09 14:11:20 +0530

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,7 @@
 // Export the config object 
 export const config = {
     PORT: process.env.PORT || 5000,
-    mongoURI: process.env.mongoURI || 'mongodb+srv://mydatabase:aryapassword@cluster0.5drnm.mongodb.net/auth-with-metamask?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/auth-with-metamask',
+    mongoURI: process.env.mongoURI || 'mongodb://127.0.0.1:27017/auth-with-metamask',
     mongoOptions : {
         useNewUrlParser: true,
         useUnifiedTopology: true
